79d9b7d9d3311c6de88f58b47bcc6c6ddea3d14e
haris316 <gogeto931@gmail.com>
2021-11-15 17:44:17 +0500

    Profile Done

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -26,30 +26,62 @@ function Profile({ user, setLoginUser }) {
   //     });
   // }, []);
 
-  let tempuser = user.user;
 
+  const [name,setName]=React.useState(user.user.name);
+  const [contact,setContact]=React.useState(user.user.contactNumber);
+  const [password,setPassword]=React.useState(user.user.password);
   return (
     <div className="ProfileContainer">
       <h3>User Details</h3>
       <div className="ProfileBox">
-        <img className="Profile_picture" src={tempuser.profilePicture} />
+        <img className="Profile_picture" src={user.user.profilePicture} />
         <div className="Profile_textBox">
           <div className="Profile_subTextBox">
             <div className="Profile_desc">Full Name : </div>
-            <input className="Profile_field" placeholder={tempuser.name} value={tempuser.name} onChange={(e)=>{tempuser.name=e.target.value}} />
+            <div className="Profile_field">
+            <input
+              type="text"
+              placeholder={name}
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            </div>
           </div>
           <div className="Profile_subTextBox">
             <div className="Profile_desc">Password : </div>
-            <div className="Profile_field">{tempuser.password}</div>
+            <div className="Profile_field">
+            <input
+              type="text"
+              placeholder={password}
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            </div>
           </div>
           <div className="Profile_subTextBox">
             <div className="Profile_desc">Contact Number : </div>
-            <div className="Profile_field">{tempuser.contactNumber}</div>
+            <div className="Profile_field">
+            <input
+              
+              type="text"
+              placeholder={contact}
+              value={contact}
+              onChange={(e) => {
+                setContact(e.target.value);
+              }}
+            />
+            </div>
           </div>
         </div>
       </div>
       <div className="profile_button"> Update Details </div>
-      <div className="profile_button" onClick={() => setLoginUser(null)}>Logout</div>
+      <div className="profile_button" onClick={() => setLoginUser(null)}>
+        Logout
+      </div>
     </div>
   );
 }
